8d43b53918261daf77e24cecde2dcc1e45032d6f
madbyit <madbyit@bystedts.com>
2022-01-23 09:37:36 +0100

    Base draft

---
+++ CentiroHomeAssignment/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentiroHomeAssignment.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            // TODO: Return all orders to a view
+
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetByOrderNumber(string orderNumber)
+        {
+            // TODO: Return the specific order to a view
+
+            throw new NotImplementedException();
+        }
+    }
+}
