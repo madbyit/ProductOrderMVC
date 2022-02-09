3d1ecc42b6906e964bf541e73d08ce7e0260411a
madbyit <madbyit@bystedts.com>
2022-02-09 21:02:35 +0100

    Move privacy to its own controller

---
+++ ProductOrderMVC/Controllers/Privacy.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductOrderWebApp.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
