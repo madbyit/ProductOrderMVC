c49df1db65bd1f1fce2d2c8405260fc086aec8c4
madbyit <madbyit@bystedts.com>
2022-03-04 08:10:07 +0100

    Clean up unused code

--- ProductOrderMVC/Controllers/OrdersController.cs
+++ ProductOrderMVC/Controllers/OrdersController.cs
@@ -26,8 +26,6 @@ namespace ProductOrderWebApp.Controllers
         {
             /// Return all orders to a view
             GetAllData();
-            //StreamReadCSV();
-            
             ViewData["OrderList"] = _orderlist;
             return View("Index");
         }
@@ -35,6 +33,8 @@ namespace ProductOrderWebApp.Controllers
         [HttpPost]
         public IActionResult GetByOrderNumber(string orderNumber)
         {
+            /// Return specific order to a view
+            
             dbpo = new DbProductOrders();
 
             List<string> str_list = new List<string>();
@@ -80,79 +80,6 @@ namespace ProductOrderWebApp.Controllers
                 customerOrderList.Add(str_list);
             }
             PublishOrderList(customerOrderList);
-        }
-
-        public void StreamReadCSV()
-        {
-            string filePath = String.Empty;
-            filePath = Environment.CurrentDirectory + "/App_Data/";
-            string[] AllFiles = Directory.GetFiles(@filePath);
-
-            for (int i = 0; i < AllFiles.Length; i++)
-            {
-                /*Read the file as one string.*/
-                using StreamReader sr = new StreamReader(AllFiles[i]);
-                var customerOrderList = new List<List<string>>();
-
-                /* Jumping first line since it is the header */
-                string headerLine = sr.ReadLine();
-
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    string csvData = line;
-
-                    /* Split by row / ordered item */
-                    foreach (string row in csvData.Split("\n"))
-                    {
-                        var rowList = new List<string>();
-                        /* Split by pipe / order description */
-                        foreach (string word in row.Split('|'))
-                        {
-                            rowList.Add(word);
-                        }
-
-                        customerOrderList.Add(rowList);
-                    }
-                }
-                PopulateOrderList(customerOrderList);
-            }
-        }
-
-        private void PopulateOrderList(List<List<string>> customerOrderList)
-        {
-            /* 
-             * The first [0] is the index 0 in the parent-list.
-             * This task included 3 files with different orders.
-             * 
-             * I want to create the orders seperated from each file.
-             * The parent list is the three files, and in each of them
-             * was the list with orders.
-            */
-            var order = new OrdersModel
-            {
-                OrderNumber = customerOrderList[0][1],
-                OrderDate = Convert.ToDateTime(customerOrderList[0][9]).Date,
-                CustomerName = customerOrderList[0][10],
-                CustomerNumber = customerOrderList[0][11],
-                OrderItems = new List<OrderItem>()
-            };
-
-            foreach (var list in customerOrderList)
-            {
-                order.OrderItems.Add(new OrderItem
-                {
-                    OrderLineNumber = Convert.ToInt32(list[2]),
-                    ProductNumber = list[3],
-                    Quantity = list[4],
-                    Name = list[5],
-                    Description = list[6],
-                    Price = list[7],
-                    ProductGroup = list[8],
-                });
-            }
-
-            _orderlist.Add(order);
         }
 
         private void PublishOrderList(List<List<string>> customerOrderList)
