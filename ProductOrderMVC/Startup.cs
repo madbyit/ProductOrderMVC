f6723e123d4599289a0d703588134f656d23861c
madbyit <madbyit@bystedts.com>
2022-05-10 20:56:37 +0200

    Move order page to first page

--- ProductOrderMVC/Startup.cs
+++ ProductOrderMVC/Startup.cs
@@ -50,7 +50,7 @@ namespace ProductOrderWebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Orders}/{action=Index}/{id?}");
             });
         }
     }
